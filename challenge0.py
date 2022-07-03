c2dc2eaf2a6d246b829d7d7822bc275b705266cd
TheVulc4n <73201814+Nadav-Nir@users.noreply.github.com>
2022-07-02 20:32:09 -0400

    Initial commit + challenge 0

---
+++ challenge0.py
@@ -0,0 +1,8 @@
+'''
+URL: http://www.pythonchallenge.com/pc/def/0.html
+
+2^38 is 274877906944
+
+So we change 'http://www.pythonchallenge.com/pc/def/0.html'
+To 'http://www.pythonchallenge.com/pc/def/274877906944.html'
+'''
